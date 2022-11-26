4ebe93374c63788680a5383acba6029a9986feb6
Amric Trudel <amrictrudel.42@gmail.com>
2022-11-26 23:18:58 +0100

    modify data augmentation

--- data.py
+++ data.py
@@ -1,8 +1,9 @@
-import torch
 import torchvision.transforms as transforms
-from torchvision import datasets
-from model import IMSIZE
+from matplotlib import pyplot as plt
 from torch.utils.data import DataLoader
+from torchvision import datasets
+
+from model import IMSIZE
 
 # once the images are loaded, how do we pre-process them before being passed into the network
 # by default, we resize the images to 64 x 64 in size
@@ -11,8 +12,12 @@ from torch.utils.data import DataLoader
 
 data_transforms = {
     'train': transforms.Compose([
-        transforms.RandomResizedCrop(224),
+        transforms.RandomResizedCrop(224, scale=(0.7, 1), ratio=(1,1)),
+        # transforms.Resize(IMSIZE),
+        # transforms.CenterCrop(224),
         transforms.RandomHorizontalFlip(),
+        transforms.RandomVerticalFlip(0.1),
+        transforms.GaussianBlur(9, (0.1, 2)),
         transforms.ToTensor(),
         transforms.Normalize(
             mean=[0.485, 0.456, 0.406],
@@ -47,4 +52,26 @@ def get_data_loader(directory: str, phase: str, batch_size: int) -> DataLoader:
     )
     return data_loader
 
+def visualize_dataset(phase: str):
+    from visualization import imshow
+    num_images = 6
+    data_loader = get_data_loader('bird_dataset', phase, num_images)
+    images_so_far = 0
+    fig = plt.figure(figsize=(10, 2 * num_images))
 
+    for i, (inputs, labels) in enumerate(data_loader):
+        for j in range(inputs.size()[0]):
+            images_so_far += 1
+            ax = plt.subplot(num_images // 2, 2, images_so_far)
+            ax.axis('off')
+            imshow(inputs.cpu().data[j])
+
+            if images_so_far > 0 and images_so_far % num_images == 0:
+                plt.tight_layout()
+                plt.show()
+                images_so_far = 0
+                text = input("Press enter to continue, q to quit")
+                if text == 'q':
+                    return
+if __name__ == '__main__':
+    visualize_dataset('val')
