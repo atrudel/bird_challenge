6e32d3eec793cdf005ec07111d6787e3f40dc9c0
Amric Trudel <amrictrudel.42@gmail.com>
2022-11-28 18:52:07 +0100

    update taining pipeline with small batch size

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ from datetime import datetime
 parser = argparse.ArgumentParser(description='RecVis A3 training script')
 parser.add_argument('--data', type=str, default='bird_dataset', metavar='D',
                     help="folder where data is located. train_images/ and val_images/ need to be found in the folder")
-parser.add_argument('--batch-size', type=int, default=64, metavar='B',
+parser.add_argument('--batch-size', type=int, default=4, metavar='B',
                     help='input batch size for training (default: 64)')
 parser.add_argument('--epochs', type=int, default=10, metavar='N',
                     help='number of epochs to train (default: 10)')
-parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
-                    help='learning rate (default: 0.01)')
+parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
+                    help='learning rate (default: 0.001)')
 parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
                     help='SGD momentum (default: 0.9)')
 parser.add_argument('--seed', type=int, default=1, metavar='S',
@@ -39,7 +39,7 @@ def setup_experiment(args) -> str:
         print("Creating an experiment folder")
         os.makedirs(args.experiment)
     os.makedirs(experiment_path)
-    print(f"Lauching experiment {experiment_name} for {args.epochs} epochs with LR={args.lr}, Momentum={args.momentum}, Scedul={args.scheduler_steps}steps")
+    print(f"Lauching experiment {experiment_name} for {args.epochs} epochs with LR={args.lr}, Momentum={args.momentum}, Schedule={args.scheduler_steps}steps")
     return experiment_path
 
 
